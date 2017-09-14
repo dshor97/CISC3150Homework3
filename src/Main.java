e665dc62e4f12bcaf99dfe061e2930d3b3ae6333
Dayton Shor <dayton.shor@gmain.com>
2017-09-14 16:58:06 -0400

    Add Main

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,53 @@ public class Main {
 }
 
 class MyString{
-    char[] charStr;
-    int length;
+    private char[] charStr;
 
-    
+    public MyString(char[] chars){
+
+    }
+    public char charAt(int index){
+        char tmp = charStr[index];
+        return tmp;
+    }
+    public int length(){
+        return charStr.length;
+    }
+    public MyString substring(int begin, int end){
+        char[] tmp = new char[end - begin];
+
+        for(int i = 0; i<(end - begin); i++){
+            tmp[i] = charStr[begin + i];
+        }
+
+        MyString myStr = new MyString(tmp);
+
+        return myStr;
+    }
+    /*
+
+    public MyString toLowerCase(){
+        char[] temp = new char[charStr.length];
+
+    }
+    public MyString toUpperCase(){
+
+    }
+    public boolean equals(MyString s){
+
+    }
+    public MyString getMyString(){
+
+    }
+    public String toString(){
+
+    }
+    public static MyString valueOf(int i){
+
+    }
+
+    */
+
+
 
 }
