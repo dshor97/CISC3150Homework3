6c343f10951161bb3e73b265b34e58aa144ea3d3
Dayton Shor <dayton.shor@gmain.com>
2017-09-14 17:15:14 -0400

    Updates

--- src/Main.java
+++ src/Main.java
@@ -8,34 +8,40 @@ class MyString{
     private char[] charStr;
 
     public MyString(char[] chars){
-
+        charStr = new char[chars.length];
+        for(int i = 0; i < chars.length; i++){
+            charStr[i] = chars[i];
+        }
     }
+
     public char charAt(int index){
         char tmp = charStr[index];
         return tmp;
     }
+
     public int length(){
         return charStr.length;
     }
+
     public MyString substring(int begin, int end){
         char[] tmp = new char[end - begin];
-
         for(int i = 0; i<(end - begin); i++){
             tmp[i] = charStr[begin + i];
         }
-
         MyString myStr = new MyString(tmp);
-
         return myStr;
     }
+
     /*
     public MyString toLowerCase(){
         char[] temp = new char[charStr.length];
 
     }
+
     public MyString toUpperCase(){
 
     }
+
     */
     public boolean equals(MyString s){
         if(charStr.length != s.charStr.length){
@@ -49,19 +55,22 @@ class MyString{
         }
         return true;
     }
+
     public MyString getMyString(){
         char[] temp = new char[charStr.length];
         for(int i = 0;i<charStr.length;i++){
             temp[i] = charStr[i];
         }
-
         MyString myStr = new MyString(temp);
-
         return myStr;
     }
-    public String toString(){
 
+    public String toString(){
+        String temp = new String(charStr);
+
+        return temp;
     }
+    
     public static MyString valueOf(int i){
 
     }
